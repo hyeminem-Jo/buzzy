8bdf51da9779197709a5d50c372430fefde58407
hyeminem-Jo <konnimey@naver.com>
2023-02-07 10:21:15 +0900

    🎉 Begin project

---
+++ prepare/front/pages/profile.js
@@ -0,0 +1,11 @@
+import AppLayout from '../components/AppLayout';
+
+const profile = () => {
+  return (
+    <AppLayout>
+    <div>내 프로필</div>
+    </AppLayout>
+  );
+};
+
+export default profile;
